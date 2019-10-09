55fe8c6c530429afb8c2a71c15121841eb5916fd
Oparah Dimkpa <oparahdc@gmail.com>
2019-10-09 03:59:14 +0100

    Add database models

--- src/server.js
+++ src/server.js
@@ -1,3 +1,43 @@
-export default function sum(a, b) {
-    return a + b;
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connect } from 'mongoose';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+
+// enable morgan logs only in development environment
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
+
+// enable use of dotenv config file.
+dotenv.config();
+
+// Connect to MongoDB
+connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+) // Let us remove that nasty deprecation warrning :)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+app.use(express.json());
+
+// Handling unavailable routes
+app.all('*', (req, res) =>
+  res.status(405).json({
+    error: 'Method not allowed',
+  }),
+);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
